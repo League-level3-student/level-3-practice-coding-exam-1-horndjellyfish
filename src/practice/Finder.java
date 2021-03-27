8455571c463132431143bb586ef5a13a241d282a
horndjellyfish <hornedjellyfish@gmail.com>
2021-03-27 11:22:56 -0700

    practice B - complete
    day 3

--- src/practice/Finder.java
+++ src/practice/Finder.java
@@ -6,12 +6,19 @@ import java.util.List;
 public class Finder {
 
     public int lastIndexOf(ArrayList<String> s, String a) {
-
+        int lastIndex = -1;
         for (int i = 0; i < s.size(); i++) {
-            
-        }
-        return -1;
-    }
+            if (s.get(i).toLowerCase().equals(a.toLowerCase())) {
+                lastIndex = i;
+                System.out.println(lastIndex);
+                //return lastIndex;
+            }
 
+        }
+        if (lastIndex == -1) {
+            return -1;
+        }
+        return lastIndex;
+    }
 
 }
